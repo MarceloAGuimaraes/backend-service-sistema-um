5b96c72ef4a8cb09f7233c8033d6a3adefab39e6
--local <--local>
2021-03-20 15:43:41 -0300

    function to address param

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
   belongs_to :request
+
+  def to_param
+    "#{space_to_plus(street)},#{space_to_plus(neighborhood)}, #{space_to_plus(city)}, #{space_to_plus(uf)}"
+  end
+
+  private
+
+  def space_to_plus(value)
+    value.gsub(' ', '+')
+  end
 end
